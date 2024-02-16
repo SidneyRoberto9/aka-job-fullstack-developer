b19c8745f921192f8547347ce089a8c70dc2fd48
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-02-16 19:08:04 -0300

    🎉 initial: Setup Server Side

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import { app } from '@/app';
+
+const port = Number(process.env.PORT || 3000);
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: port,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!');
+  });
