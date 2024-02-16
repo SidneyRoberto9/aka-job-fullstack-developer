1067318652f91eeaf2610742920b0ad518fe077d
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-02-16 18:58:47 -0300

    🎉 initial: Setup Client Side

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+
+import '@/styles/globals.css';
+
+import { PropsWithChildren } from 'react';
+import { Roboto } from 'next/font/google';
+import Favicon from '/public/favicon.ico';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
+
+export const metadata: Metadata = {
+  title: 'AKASOFT - Cotação do Dólar',
+  description: 'Desafio técnico para a vaga de desenvolvedor fullstack na AKASOFT',
+  icons: [{ rel: 'icon', url: Favicon.src }],
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
