5c6dcf6d1cfdd942c8390e92468b4a42acffcb3e
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-02-18 17:02:45 -0300

    📦 refactor: Remove Google Login Button from register and Login

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import { isAuthenticateRedirect } from '@/server/auth';
-import { GoogleButton } from '@/components/GoogleButton';
 import { CenterBox } from '@/components/CenterBox';
 import { LoginForm } from '@/components/forms/loginForm';
 
@@ -13,18 +12,8 @@ export default async function Page() {
       <article className="flex items-center justify-center flex-col gap-6 max-w-72 w-72 md:w-96 md:max-w-96 ">
         <section className="text-center flex flex-col gap-2">
           <h1 className="font-semibold text-2xl ">Sign In</h1>
-          <p className="text-sm opacity-45">Your Social Campaigns</p>
+          <p className="text-sm opacity-45">Your journey begins here</p>
         </section>
-
-        <section className="w-full flex items-center justify-center">
-          <GoogleButton text="Sign In With Google" />
-        </section>
-
-        <div className="flex w-full items-center justify-center">
-          <span className="border-b border-black opacity-10 w-1/3 " />
-          <p className="px-2 md:px-4 opacity-40 text-xs">Or with Email</p>
-          <span className="border-b border-black opacity-10 w-1/3" />
-        </div>
 
         <LoginForm />
 
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import { GoogleButton } from '@/components/GoogleButton';
 import { RegisterForm } from '@/components/forms/registerForm';
 
 export default function Page() {
@@ -8,18 +7,8 @@ export default function Page() {
     <article className="flex items-center justify-center flex-col gap-6 max-w-72 w-72 md:w-96 md:max-w-96">
       <section className="text-center flex flex-col gap-2">
         <h1 className="font-semibold text-2xl">Sign Up</h1>
-        <p className="text-sm opacity-45">Your Social Campaigns</p>
+        <p className="text-sm opacity-45">Join our community today</p>
       </section>
-
-      <section className="w-full flex items-center justify-center">
-        <GoogleButton text="Sign Up With Google" />
-      </section>
-
-      <div className="flex w-full items-center justify-center">
-        <span className="border-b border-black opacity-10 w-1/3" />
-        <p className="px-2 md:px-4 opacity-40 text-xs">Or with Email</p>
-        <span className="border-b border-black opacity-10 w-1/3" />
-      </div>
 
       <RegisterForm />
 
