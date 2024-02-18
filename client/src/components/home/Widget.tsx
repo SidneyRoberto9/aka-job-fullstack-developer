21f54cb97842885820ff8a7309ca39b3ececaf70
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-02-18 01:28:56 -0300

    ✨ feat: Create Widget Component with react countup

--- client/src/components/home/Widget.tsx
+++ client/src/components/home/Widget.tsx
@@ -1,17 +1,28 @@
+'use client';
+import CountUp from 'react-countup';
 import React from 'react';
 
 interface WidgetProps {
   name: string;
-  value: number;
+  value: string;
 }
 
 export function Widget({ name, value }: WidgetProps) {
   return (
-    <div className="w-11/12 h-full rounded-lg shadow-md m-2 bg-white/30 select-none">
+    <div className="w-[95%] h-32 sm:h-full rounded-lg shadow-md m-2 bg-white/30 select-none">
       <div className="w-full h-full flex flex-col items-center justify-center">
         <div>
           <span className="text-lg">{name}</span>
-          <h1 className="text-5xl font-semibold">{value}</h1>
+          <h1 className="text-5xl font-semibold">
+            <CountUp
+              end={Number(value)}
+              delay={0.25}
+              duration={2}
+              separator=""
+              decimal=","
+              decimals={4}
+            />
+          </h1>
         </div>
       </div>
     </div>
