55cef6d438e65a06d89b7d58dfb3715b616a9513
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-02-18 15:18:39 -0300

    ✏️ docs: Update listDocs endpoint in exchange-rate controller

--- server/src/http/controller/exchange-rate/list/list.docs.ts
+++ server/src/http/controller/exchange-rate/list/list.docs.ts
@@ -5,12 +5,22 @@ export const listDocs = {
       'The list endpoint retrieves a paginated list of dollar quotations, with optional filters for page number and date to customize the data retrieval.',
     tags: ['Exchange Rate'],
     summary: 'List Exchange Rate Data',
-    querystring: {
+    body: {
       type: 'object',
       properties: {
-        page: { type: 'integer', description: 'Page number for pagination' },
-        to: { type: 'string', format: 'date', description: 'Date for filtering quotations' },
-        from: { type: 'string', format: 'date', description: 'Date for filtering quotations' },
+        page: { type: 'integer', description: 'Page number for pagination', default: 1 },
+        to: {
+          type: 'string',
+          format: 'date',
+          description: 'Date for filtering quotations',
+          default: 'optional (remove this line if not use)',
+        },
+        from: {
+          type: 'string',
+          format: 'date',
+          description: 'Date for filtering quotations',
+          default: 'optional (remove this line if not use)',
+        },
       },
     },
   },
